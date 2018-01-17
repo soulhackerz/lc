7d7e647d9076b18c6b68ba62ffd29410e8571394
soulhackerz <soulgi@gmail.com>
2018-01-16 23:21:05 -0800

    add questions

--- LCode/src/lcode/solution/LAmzUtil.java
+++ LCode/src/lcode/solution/LAmzUtil.java
@@ -39,4 +39,29 @@ public class LAmzUtil {
         }
         return -1;
     }
+    
+    /**
+     * 344. Reverse String
+     * https://leetcode.com/problems/reverse-string/description/
+     */
+    public String reverseString(String s) {
+        return "";
+    }
+    
+    /**
+     * 186. Reverse Words in a String II
+     * 
+     * https://leetcode.com/problems/reverse-words-in-a-string-ii/description/
+     */
+    public void reverseWords(char[] str) {
+
+    }
+    
+    /**
+     * 1. Two Sum
+     * https://leetcode.com/problems/two-sum/
+     */
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
 }
