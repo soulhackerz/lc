f9f624cebe04aeb1a917968d0ec1f9e6b2378b2d
soulhackerz <soulgi@gmail.com>
2018-04-23 01:05:09 -0700

    Find duplicate.

--- LCode/src/lcode/solution/LAmzUtil.java
+++ LCode/src/lcode/solution/LAmzUtil.java
@@ -326,7 +326,21 @@ public class LAmzUtil {
      * @return 
      */
     public int findDuplicate(int[] nums) {
-        return -1;
+        int i = 0;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
     
     /**
--- LCode/src/lcode/test/LAmzUtilTest.java
+++ LCode/src/lcode/test/LAmzUtilTest.java
@@ -117,4 +117,23 @@ public class LAmzUtilTest {
         System.out.println(lau.numberToWords(12345));
         System.out.println(lau.numberToWords(1234567));
     }    
+    
+    /**
+     * 287. Find the Duplicate Number
+     * https://leetcode.com/problems/find-the-duplicate-number/description/
+     * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+        Note:
+        You must not modify the array (assume the array is read only).
+        You must use only constant, O(1) extra space.
+        Your runtime complexity should be less than O(n2).
+        There is only one duplicate number in the array, but it could be repeated more than once.
+     * @param nums
+     * @return 
+     */
+    
+    public static void test287() {
+        LAmzUtil lau = new LAmzUtil();
+        System.out.println(lau.findDuplicate(new int[] {1,3,4,2,2,}));
+    }
 }
