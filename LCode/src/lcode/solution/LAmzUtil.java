21ae076a3d6cc3e3b0da9d44310c71ead5c9bdcf
soulhackerz <soulgi@gmail.com>
2018-04-22 23:45:46 -0700

    Integer to English words.

--- LCode/src/lcode/solution/LAmzUtil.java
+++ LCode/src/lcode/solution/LAmzUtil.java
@@ -215,6 +215,7 @@ public class LAmzUtil {
         return result;
     }    
     
+    
     /**
      * 273. Integer to English Words
      * https://leetcode.com/problems/integer-to-english-words/description/
@@ -226,9 +227,90 @@ public class LAmzUtil {
      * @param num
      * @return 
      */
+    public Map<Integer, String> map = new HashMap();
     public String numberToWords(int num) {
-        return "";
-    }   
+        populateMap();
+        if (num == 0) {
+            return map.get(num);
+        }
+        String result = "";
+        if (num >= 1000000000) {
+            int rnum = num/1000000000;
+            result += convertToWords(rnum) + " Billion";
+            num = num%1000000000;
+        }
+        if (num >= 1000000) {
+            int rnum = num/1000000;
+            result += " " + convertToWords(rnum) + " Million";
+            num = num%1000000;
+        } 
+        if (num >= 1000) {
+            int rnum = num/1000;
+            result += " " + convertToWords(rnum) + " Thousand";
+            num = num%1000;
+        } 
+        if (num > 0){
+            result += " " + convertToWords(num);
+        }           
+        return result.trim();          
+        
+    }
+    
+    private String convertToWords(int num) {
+        String result = "";
+        if (num >= 100) {
+            result += map.get(num/100) + " Hundred";
+            int remainder = num%100;
+            if (remainder > 0)
+                result += " " +convertSmallToWords(remainder);
+
+        } else {
+            result += convertSmallToWords(num);
+        }
+        return result;
+    }
+    private String convertSmallToWords(int num) {
+        String result = "";
+        if (map.containsKey(num)) {
+            result += map.get(num);
+        } else if (num > 20) {
+            int remainder = num % 10;
+            int mapnum = num - remainder;
+            result += map.get(mapnum)+ " " + map.get(remainder);
+        }
+        return result;
+    }         
+    
+    private void populateMap() {
+        map.put(0, "Zero");
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+        map.put(6, "Six");
+        map.put(7, "Seven");
+        map.put(8, "Eight");
+        map.put(9, "Nine");
+        map.put(10, "Ten");
+        map.put(11, "Eleven");
+        map.put(12, "Twelve");
+        map.put(13, "Thirteen");
+        map.put(14, "Fourteen");
+        map.put(15, "Fifteen");
+        map.put(16, "Sixteen");
+        map.put(17, "Seventeen");
+        map.put(18, "Eighteen");
+        map.put(19, "Nineteen");
+        map.put(20, "Twenty");
+        map.put(30, "Thirty");
+        map.put(40, "Forty");
+        map.put(50, "Fifty");
+        map.put(60, "Sixty");
+        map.put(70, "Seventy");
+        map.put(80, "Eighty");
+        map.put(90, "Ninety");
+    }
     
     /**
      * 287. Find the Duplicate Number
